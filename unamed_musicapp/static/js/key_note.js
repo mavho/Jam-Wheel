51c353cafb9cab49bb3bebfb1d93cdcc4d1c9148
mavHo <homaverick@gmail.com>
2020-04-30 20:21:25 -0700

    fixed messy code, able to change height of keycirc, added throbbing lights baby

--- unamed_musicapp/static/js/key_note.js
+++ unamed_musicapp/static/js/key_note.js
@@ -6,17 +6,16 @@ class KeyNote{
 
     show is continusously called
     */
-    constructor (x1,y1,x2,y2,x3,y3, in_color,note){
+    constructor (x1,y1,x2,y2,x3,y3, in_color="#E1008E",note){
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
         this.x3 = x3;
         this.y3 = y3;
-        this.color = in_color;
-        this.curr_color = this.color;
+        this.orig_color = in_color;
+        this.curr_color = this.orig_color;
         this.note = note;
-        this.brightness = 0;
         this.noiseSynth = new Tone.DuoSynth().toMaster();
         this.vibratoAmount = 12;
     }   
@@ -34,20 +33,20 @@ class KeyNote{
         return s > 0 && t > 0 && (s + t) < 2 * A * sign;
     }
 
-    clicked(vibrato){
-        let color_shade = lerpColor(color(this.color), color(72,61,139), 0.35);
+    clicked(){
+        let color_shade = lerpColor(color(this.orig_color), color(72,61,139), 0.35);
         this.curr_color = color_shade;
         this.trigger_sound();
     }
 
     dragged(vibrato){
-        this.vibratoAmount = vibrato * 3;
-        let color_shade = lerpColor(color(this.color), color(72,61,139), 0.35);
+        this.vibratoAmount = vibrato*10;
+        let color_shade = lerpColor(color(this.orig_color), color(72,61,139), 0.35);
         this.curr_color = color_shade;
         this.trigger_sound();
     }
     released(){
-        this.curr_color = this.color;
+        this.curr_color = this.orig_color;
         this.noiseSynth.triggerRelease();
     }
 
--- unamed_musicapp/static/js/manage_shapes.js
+++ unamed_musicapp/static/js/manage_shapes.js
@@ -1,61 +1,78 @@
-hex_colors = ["#E50018","#E1008E","#BA00DD","#4400D9","#002CD5","#009AD2","#00CE98","#00CA2A","#3EC600","#A3C200"]
+hex_color_wheel = ["#E50018","#E1008E","#BA00DD","#4400D9","#002CD5","#009AD2","#00CE98","#00CA2A","#3EC600","#A3C200"]
+
+hex_color_blue = ["#0016E5","#0229DB","#053AD2","#0749C9","#0956BF","#0B61B6","#0C6AAD","#0E71A3","#0F779A","#107A91"]
 
 C_MAJ_SCALE =["C4","D4","E4","F4","G4","A4","B4","C5","D5","E5"]
 
 //save the state of each key.
 let keys = [];
 
+var synth = new Tone.MembraneSynth().toMaster()
 function setup(){
     // Set up canvas
-    var canvas = createCanvas(500,500);
+    var can_width = 600;
+    var can_height=600;
+    var canvas = createCanvas(can_width,can_height);
+
     canvas.parent('sketch')
     canvas.style('display','block')
+    background(255,69,0);
 
     //Vars for the circle
     points = 10;
     pointAngle = 360/points;
-    radius = width/2;
-    x = height/2;
-    y = width/2;
 
-    counter = 0 
+    radius = can_width/2.5;
+
+    cir_centerX= can_width/2;
+    cir_centerY = can_height/2;
+
+    let counter = 0 
     //draw the triangle circle
     for (let angle=270;angle<630;angle=angle+pointAngle){
         x = cos(radians(angle)) * radius; //convert angle to radians for x and y coordinates
         y = sin(radians(angle)) * radius;
+
         let temp = angle + pointAngle;
-        var tri = new KeyNote(radius,radius,x+radius,y+radius,((cos(radians(temp)) * radius) + radius),(sin(radians(temp)) * radius) + radius, hex_colors[counter],C_MAJ_SCALE[counter]);
+        var tri = new KeyNote(cir_centerX,cir_centerY,x+cir_centerX,y+cir_centerY,((cos(radians(temp)) * radius) + cir_centerX),(sin(radians(temp)) * radius) + cir_centerY, hex_color_blue[counter],C_MAJ_SCALE[counter]);
         counter++;
         keys.push(tri);
     }
-    noStroke();
     document.querySelector('button').addEventListener('click', async () => {
         await Tone.start()
         console.log('audio is ready')
+        Tone.Transport.toggle();
     });
-
-    var synth = new Tone.MembraneSynth().toMaster()
-
     //create a loop
     var loop = new Tone.Loop(function(time){
         synth.triggerAttackRelease("C1", "8t", time)
-    }, "4t")
+    }, "4n")
     
     //play the loop between 0-2m on the transport
-    loop.start(0).stop('2m')
-
-    //Tone setup
-    //set the transport to repeat
+    loop.start(0).stop('1m')
     Tone.Transport.loopEnd = '1m'
     Tone.Transport.loop = true
-
+    //Start the loop
     Tone.Transport.toggle();
 }
 
-
-// this function is called repeatedly
+//Draw the shapes continuously
 function draw(){
-    stroke('black')
+    background("#003680");
+    var r = 150;
+    noFill();
+    stroke(197,185,166);
+    strokeWeight(5);
+    //let b = pressed_key.noiseSynth.voice0.envelope.value;
+    let b = synth.envelope.value;
+    for (var i = 0; i < 3; i++){
+        x2 = (r + b*200)*tan(2*PI/b);
+        y2 = (r + b*200)*tan(2*PI/b);
+        ellipse(height/2,width/2, x2, y2);
+    }
+
+    stroke(94, 131, 181);
+    strokeWeight(2);
     for(let i = 0; i < keys.length; i++){
         keys[i].show();
     }
@@ -71,7 +88,6 @@ function mousePressed(){
     }
     return false;
 }
-
 
 function mouseDragged(event){
     for(let key of keys){
