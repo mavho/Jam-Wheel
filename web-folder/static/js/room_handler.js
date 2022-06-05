d70c548cb2f2ab49745cb4d30f083eea3f2bb877
Maverick <homaverick@gmail.com>
2022-06-05 18:15:10 +0000

    Fixed input verification handlers. Correct error messages are displayed on input verification.

--- web-folder/static/js/room_handler.js
+++ web-folder/static/js/room_handler.js
@@ -8,50 +8,54 @@ var users = {}
 document.querySelector("#room_join").addEventListener("click", async() =>{
     room_name = document.getElementById("room_input").value;
     user_name = document.getElementById("username_input").value;
-    if(!checkInput(room_name) || !checkInput(user_name)){
-        alert("Inputs must not be empty and at least 8 characters")
-    }else{
-        //socket.emit('join room', {'room': room_name, 'user_name': user_name});
 
-        let xhr = new XMLHttpRequest();
-        let url = `https://${document.domain}:${location.port}/register`
+    if(!checkInput(room_name,8)){
+        alert("Room name must not be empty and at least 8 characters");
+        return;
+    }
+    if(!checkInput(user_name,16)){
+        alert("Username most not be empty and at least 16 characters");
+        return;
+    }
 
-        xhr.open('POST',url);
+    let xhr = new XMLHttpRequest();
+    let url = `https://${document.domain}:${location.port}/register`
 
-        let json = JSON.stringify({
-            username: user_name,
-            //room: room_name
-        });
-        xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
-        xhr.send(json);
+    xhr.open('POST',url);
 
-        xhr.onload = () => {
-            let payload = JSON.parse(xhr.response);
+    let json = JSON.stringify({
+        username: user_name,
+        //room: room_name
+    });
+    xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+    xhr.send(json);
 
-            socket = new WebSocket(`wss://${document.domain}:${location.port}/ws/${payload['url']}`);
+    xhr.onload = () => {
+        let payload = JSON.parse(xhr.response);
 
-            socket.onopen = function(e){
-                console.log("Connection established");
+        socket = new WebSocket(`wss://${document.domain}:${location.port}/ws/${payload['url']}`);
 
-                socket.send(JSON.stringify({'topics':room_name}));
+        socket.onopen = function(e){
+            console.log("Connection established");
 
-                $.ajax(`https://${document.domain}:${location.port}/templates/_includes/jamwheel.html`).done(function(reply){
-                    $('#title').removeClass("glow").addClass("muted-glow");
-                    $('#container').html(reply);
-                });
+            socket.send(JSON.stringify({'topics':room_name}));
+
+            $.ajax(`https://${document.domain}:${location.port}/templates/_includes/jamwheel.html`).done(function(reply){
+                $('#title').removeClass("glow").addClass("muted-glow");
+                $('#container').html(reply);
+            });
+        }
+
+        socket.onclose = function(e){
+            if (e.wasClean){
+                console.log(`Connection closed cleanly, code=${e.code}, reason = ${e.reason}`);
+            }else{
+                console.log('Connection died');
             }
+        }
 
-            socket.onclose = function(e){
-                if (e.wasClean){
-                    console.log(`Connection closed cleanly, code=${e.code}, reason = ${e.reason}`);
-                }else{
-                    console.log('Connection died');
-                }
-            }
-
-            socket.onerror = function(error){
-                console.log(`error ${error.message}`);
-            }
+        socket.onerror = function(error){
+            console.log(`error ${error.message}`);
         }
     }
 });
@@ -67,9 +71,9 @@ function textCounter(field, count_field_id, limit){
         count_field.text(limit-field.value.length);
     }
 }
-function checkInput(input){
+function checkInput(input,max_len){
     //check if in is empty, blank, null, ud, or greater than 8 chars
-    if(!input || /^\s*$/.test(input) || input.length > 8){
+    if(input.length == 0 || /^\s*$/.test(input) || input.length > max_len){
         return false;
     }
     return true;
